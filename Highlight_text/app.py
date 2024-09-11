a5898e315554ba17ced21926edb5f7951bc47af9
MinhThuan10 <thuan77f1@gmail.com>
2024-09-11 20:50:32 +0700

    update

--- Highlight_text/app.py
+++ Highlight_text/app.py
@@ -39,7 +39,7 @@ def highlight_text(original_text, indices, school_id, sentence_id):
             start = i
             while i in indices and i < len(words):
                 i += 1
-            highlighted.append(f'<span class="highlight_school{school_id} highlight_sentence{sentence_id}">{" ".join(words[start:i])}</span>')
+            highlighted.append(f'<span class="highlight_school{school_id} highlight highlight_sentence{sentence_id}">{" ".join(words[start:i])}</span>')
         else:
             highlighted.append(words[i])
             i += 1
@@ -49,17 +49,25 @@ def highlight_text(original_text, indices, school_id, sentence_id):
 def index(file_id):
     try:
         plagiarism_data = []
+        plagiarism_details = []
         page_count, word_count = get_pdf_from_mongo(file_id)
         plagiarisms = sentences_collection.find({'file_id': int(file_id)})
         schools = defaultdict(lambda: {'school_name':'', 'total_word_count': 0, 'sentences': []})
+        schools_detail = defaultdict(lambda: {'school_name':'', 'total_word_count': 0, 'sentences': []})
         
         for doc in plagiarisms:
             plagiarism = []
+            plagiarism_detail = []
+
             sentence_id = doc['sentence_index']
             if doc['plagiarism'] == 'no':
-                plagiarism.append([doc['sentence'], 0, sentence_id])
+                plagiarism.append([doc['sentence'], 0, 0, sentence_id])
+                plagiarism_detail.append([doc['sentence'], 0, 0, sentence_id])
+
             else:
                 best_matches = {}
+                best_score = -1
+                best_matches_data = {}
                 for source in doc['sources']:
                     school_id = source.get('school_id', 'Unknown')
                     school_name = source.get('school_name')
@@ -72,6 +80,17 @@ def index(file_id):
                     highlighted_sentence = highlight_text(doc['sentence'], indices_sentence, school_id, sentence_id)
                     highlighted_best_match = highlight_text(best_match, indices_best_match, school_id, sentence_id)
                     
+                    # Cập nhật thông tin trường học có điểm số cao nhất
+                    if score > best_score:
+                        best_score = score
+                        best_matches_data[school_id] = {
+                            'school_name': school_name,
+                            'highlighted_sentence': highlighted_sentence,
+                            'highlighted_best_match': highlighted_best_match,
+                            'word_count_sml': word_count_sml,
+                            'score': score
+                        }
+
                     if school_id not in best_matches or best_matches[school_id]['score'] < score:
                         best_matches[school_id] = {
                             'school_name': school_name,
@@ -81,20 +100,49 @@ def index(file_id):
                             'score': score
                         }
 
+
+                # Giả sử `best_matches` là một dictionary với `school_id` là khóa và `data` là giá trị
+                for school_id, data in best_matches_data.items():
+                    # Cập nhật thông tin trường học
+                    if school_id not in schools_detail:
+                        schools_detail[school_id] = {
+                            'total_word_count': 0,
+                            'school_name': data['school_name'],
+                            'sentences': []
+                        }
+
+                    schools_detail[school_id]['total_word_count'] += data['word_count_sml']
+                    schools_detail[school_id]['sentences'].append({
+                        'best_match': data['highlighted_best_match'],
+                        'sentence': data['highlighted_sentence'],
+                        'word_count_sml': data['word_count_sml'],
+                    })              
+                    plagiarism_detail.append([data['highlighted_sentence'], data['word_count_sml'], school_id, sentence_id])
+
+                # Giả sử `best_matches` là một dictionary với `school_id` là khóa và `data` là giá trị
                 for school_id, data in best_matches.items():
-                    
+                    # Cập nhật thông tin trường học
+                    if school_id not in schools:
+                        schools[school_id] = {
+                            'total_word_count': 0,
+                            'school_name': data['school_name'],
+                            'sentences': []
+                        }
+
                     schools[school_id]['total_word_count'] += data['word_count_sml']
-                    schools[school_id]['school_name'] = data['school_name']
                     schools[school_id]['sentences'].append({
                         'best_match': data['highlighted_best_match'],
                         'sentence': data['highlighted_sentence'],
                         'word_count_sml': data['word_count_sml'],
                     })
-                    plagiarism.append([data['highlighted_sentence'], school_id, sentence_id])
-                
-                plagiarism_data.append(plagiarism)
+                    
+                    plagiarism.append([data['highlighted_sentence'], data['word_count_sml'], school_id, sentence_id])
+            
+            plagiarism_details.append(plagiarism_detail)
 
-        return render_template('index.html', plagiarisms=plagiarism_data, page_count=page_count, word_count=word_count, schools=schools)
+            plagiarism_data.append(plagiarism)
+
+        return render_template('index.html', plagiarisms=plagiarism_data, plagiarism_details=plagiarism_details, page_count=page_count, word_count=word_count, schools=schools, schools_detail=schools_detail)
 
     except Exception as e:
         print(f"Error: {e}")
